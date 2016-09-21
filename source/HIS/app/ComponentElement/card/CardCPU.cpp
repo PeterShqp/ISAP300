8dfc00a38cf6141d0551903fd8b78f46ac7170b4
Peter <momo_nono@163.com>
2016-09-21 12:29:38 +0800

    分支测试，初步正常，版本号更改为3.1.8

--- source/HIS/app/ComponentElement/card/CardCPU.cpp
+++ source/HIS/app/ComponentElement/card/CardCPU.cpp
@@ -12,7 +12,7 @@
 
 static const uint8 MainVerNumberX = 3;
 static const uint8 MainVerNumberY = 1;
-static const uint8 MainVerNumberZ = 7;
+static const uint8 MainVerNumberZ = 8;
 
 CardCPU::CardCPU() {
 }
