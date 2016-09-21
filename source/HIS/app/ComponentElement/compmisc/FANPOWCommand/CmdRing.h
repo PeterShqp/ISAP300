299fd38acad452e90f912a60fcf94a7cbcace073
Peter <momo_nono@163.com>
2016-09-21 12:06:49 +0800

    与newcli分支合并

---
+++ source/HIS/app/ComponentElement/compmisc/FANPOWCommand/CmdRing.h
@@ -0,0 +1,18 @@
+/*
+ * CmdRing.h
+ *
+ *  Created on: 2016年9月20日
+ *      Author: Administrator
+ */
+
+#ifndef SOURCE_HIS_APP_COMPONENTELEMENT_COMPMISC_FANPOWCOMMAND_CMDRING_H_
+#define SOURCE_HIS_APP_COMPONENTELEMENT_COMPMISC_FANPOWCOMMAND_CMDRING_H_
+
+class CmdRing: public VFCommand {
+    CmdRing();
+public:
+    CmdRing();
+    virtual ~CmdRing();
+};
+
+#endif /* SOURCE_HIS_APP_COMPONENTELEMENT_COMPMISC_FANPOWCOMMAND_CMDRING_H_ */
