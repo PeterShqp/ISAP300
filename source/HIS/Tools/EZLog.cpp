9dba8a4c716c67749ba62e7b436feb601b91be49
PeterShi <momo_nono@163.com>
2017-04-13 17:23:41 +0800

    尝试修改日志机制，未成功，待日后修改

--- source/HIS/Tools/EZLog.cpp
+++ source/HIS/Tools/EZLog.cpp
@@ -15,29 +15,27 @@ OS_MUT mut_log;
 const static char logAname[] = "A.log";
 const static char logBname[] = "B.log";
 
+TASK void saveLog();
+
 void EZLog::record(std::string& txt) {
-	record(txt.c_str());
+
+    os_mut_wait(mut_log, 0xffff);
+    BufOfLog[bp++] = txt;
+    os_mut_release(mut_log);
+    os_evt_set(0x0001, task_save);
 }
 
 void EZLog::record(const char* s) {
-    os_mut_wait(mut_log, 0xffff);
-    FILE* f = fopen(curFile, "a");
-    if( f ) {
-        fprintf(f, "%d ", os_time_get());
-        fputs(s, f);
-        fputc('\n', f);
-        fclose(f);
-        if(ifFileFull(curFile)) {
-        	switchCurrFile();
-        }
-    }
-    os_mut_release(mut_log);
+	std::string txt(s);
+	record(txt);
 }
 
 void EZLog::init(void) {
     os_mut_init(mut_log);
     MAX_SIZE = 6000;
+    bp = 0;
     findActiveFile();
+    task_save = os_tsk_create(saveLog, P_SAVE+1);
 }
 
 void EZLog::findActiveFile(void) {
@@ -86,4 +84,42 @@ bool EZLog::ifFileFull(const char* f) {
 }
 bool EZLog::ifFileExist(const char* f) {
 	return CPPTools::getFileSize(f) >= 0;
+}
+
+void EZLog::saveToFile(void) {
+    FILE* f = fopen(curFile, "a");
+    if( f ) {
+        fprintf(f, "%d ", os_time_get());
+        os_mut_wait(mut_log, 0xffff);	//存储期间保护临界资源
+        for (int i = 0; i < bp; ++i) {
+            fputs(BufOfLog[i].c_str(), f);
+		}
+        bp = 0;							//存储完毕后重置buf指针
+        BufOfLog.clear();
+        os_mut_release(mut_log);
+        fputc('\n', f);
+        fclose(f);
+        if(ifFileFull(curFile)) {
+        	switchCurrFile();
+        }
+    }
+
+}
+
+TASK void saveLog() {
+    OS_RESULT result;
+    bool needSave = false;
+    while( 1 ) {
+        result = os_evt_wait_or(0x0001, 1000);
+        if( ((result == OS_R_TMO) && needSave) ||
+        		EZLog::instance().bufFull() ) {
+        	//系统在10秒没有新的日志产生，或者日志buf满的时候存储到文件
+            EZLog::instance().saveToFile();
+            needSave = false;
+        }
+        else if( result == OS_R_EVT ) {
+            needSave = true;
+        }
+    }
+
 }
--- source/HIS/include/EZLog.h
+++ source/HIS/include/EZLog.h
@@ -10,6 +10,8 @@
 
 #include <string>
 #include "EZ_types.h"
+#include <vector>
+#include "os.h"
 
 class EZLog {
     EZLog() {};
@@ -24,6 +26,15 @@ public:
     void record(std::string& txt);
     void record(const char* s);
 
+    bool bufFull(void) {
+    	if( bp >= MAX_BUF_SIZE ) {
+    		return true;
+    	}
+    	return false;
+    };
+    void saveToFile(void);
+
+    OS_TID task_save;
 private:
     uint32 MAX_SIZE;
     const char* curFile;
@@ -31,6 +42,9 @@ private:
     bool ifFileExist(const char* f);
     void findActiveFile(void);
     void switchCurrFile(void);
+    std::vector<std::string> BufOfLog;
+    int bp;
+    const static int MAX_BUF_SIZE = 10;
 };
 
 #endif /* SOURCE_HIS_TOOLS_EZLOG_H_ */
